a2305a560a12af20decf68ec26cffcbe959f6b35
Moataz <moatazalbarbamossa@Moatazs-MacBook-Pro.local>
2024-06-15 23:01:10 +0300

    add navbar components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import './App.css';
-import { Button } from './components/ui/button';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './lib/Pages/Navbar/Navbar';
 function App() {
-  return <Button variant="link">Click me</Button>;
+  return (
+    <Router>
+      <div className="flex bg-[#E5E5E5]">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<>interface</>} />
+          <Route path="/about" element={<>about</>} />
+          <Route path="/services" element={<>ser</>} />
+          <Route path="/contact" element={<>cont</>} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
